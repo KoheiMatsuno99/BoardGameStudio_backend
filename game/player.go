0a8630c54138ea5295e76924a80d39ed59c7a8dc
kohei <koheimtri3@gmail.com>
2023-11-18 16:16:49 +0900

    Add player struct

---
+++ game/player.go
@@ -0,0 +1,34 @@
+package game
+
+type Player struct {
+	// TODO: guidを追加する
+	name string
+	pieces map[string]Piece
+	pickedRedPiecesCount int
+	pickedBluePiecesCount int
+}
+
+func NewPlayer(name string, pieces map[string]Piece, pickedRedPiecesCount, pickedBluePiecesCount int) *Player{
+	return &Player{
+		name: name,
+		pieces: map[string]Piece{},
+		pickedRedPiecesCount: pickedRedPiecesCount,
+		pickedBluePiecesCount: pickedBluePiecesCount,
+	}
+}
+
+func (p *Player) Name() string{
+	return p.name
+}
+
+func (p *Player) Pieces() map[string]Piece{
+	return p.pieces
+}
+
+func (p *Player) PickedRedPiecesCount() int{
+	return p.pickedRedPiecesCount
+}
+
+func (p *Player) PickedBluePiecesCount() int{
+	return p.pickedBluePiecesCount
+}
