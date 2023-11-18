6b62a0ab4334bd51db76c861a10399b2b6b2a793
kohei <koheimtri3@gmail.com>
2023-11-18 15:55:37 +0900

    Add Block struct

---
+++ game/block.go
@@ -0,0 +1,21 @@
+package game
+
+type Block struct {
+	address []int
+	piece *Piece
+}
+
+func NewBlock(address []int, piece *Piece) *Block{
+	return &Block{
+		address: address,
+		piece: piece,
+	}
+}
+
+func (b *Block) Address() []int{
+	return b.address
+}
+
+func (b *Block) Piece() *Piece{
+	return b.piece
+}
