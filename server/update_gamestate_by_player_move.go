106956d5d73093f9a770cf901eb90c355702fcb6
kohei <koheimtri3@gmail.com>
2023-12-03 21:41:41 +0900

    Refactor piece deserialization and update game state

--- server/update_gamestate_by_player_move.go
+++ server/update_gamestate_by_player_move.go
@@ -12,15 +12,25 @@ func (gss *GeisterServiceServer) UpdateGameStateByPlayerMove(ctx context.Context
 	pieceKey := req.GetPieceKey()
 	serializedDest := req.GetDest()
 	gameState := gss.gameStateMap[tableUuid]
-	//　Deserializeするので、特にPieceの参照に注意
-	dest := game.NewBlock(
-		[]int{int(serializedDest.GetAddress()[0]), int(serializedDest.GetAddress()[1])},
-		game.NewPiece(
+
+	// Deserialize
+	var piece *game.Piece
+	serializedPiece := serializedDest.GetPiece()
+	if serializedPiece != nil {
+		piece = game.NewPiece(
 			serializedDest.GetPiece().GetOwner(),
 			serializedDest.GetPiece().GetPieceType(),
 			[]int{int(serializedDest.GetPiece().GetPosition()[0]), int(serializedDest.GetPiece().GetPosition()[1])},
-		),
+		)
+	}
+	dest := game.NewBlock(
+		[]int{int(serializedDest.GetAddress()[0]), int(serializedDest.GetAddress()[1])},
+		piece,
 	)
+	// Deserializeで生成したPiece, Blockで上書きする
+	gameState.Players()[gameState.Turn()].Pieces()[pieceKey] = piece
+	gameState.Board()[dest.Address()[0]][dest.Address()[1]] = dest
+
 	err := gameState.PlayerMove(pieceKey, *dest)
 	if err != nil {
 		return nil, err
